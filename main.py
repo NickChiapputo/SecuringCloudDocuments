393aff1ab05d657a2b8879417944fe09ff020c80
NickChiapputo <chiapputo.nick@gmail.com>
2019-04-14 23:31:48 -0500

    Encrypts and decrypts message using AES256. I think it MACs properly using SHA256. Unsure if MAC works properly.

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import os
+import binascii
+from base64 import b64encode
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, hmac
 
-backend = default_backend()
+### ENCRYPTION ###
+cipherKey = os.urandom( 32 )						# Random 32 byte (256 bit) key
+nonce = os.urandom( 16 )					# Create a nonce for the counter
 
-key = os.urandom( 32 )
-nonce = os.urandom( 16 )
-
-cipher = Cipher( algorithms.AES( key ), modes.CTR( nonce ), backend=backend )
-encryptor = cipher.encryptor()
+cipher = Cipher( algorithms.AES( cipherKey ), modes.CTR( nonce ), default_backend() )	# Create the Cipher
+encryptor = cipher.encryptor()													# Encrypt 
 ct = encryptor.update( b"A secret message" ) + encryptor.finalize()
-ct = ct.decode( 'UTF-8' )
 
 print( f'Cipher Text: {ct}' )
 
-decryptor = cipher.decryptor()
-decryptor.update( ct ) + decryptor.finalize()
+##################
+
+
+###    MAC     ###
+macKey = os.urandom( 32 )
+h = hmac.HMAC( macKey, hashes.SHA256(), default_backend() )
+h.update( ct )
+finalCipher = h.finalize()
+
+print( f'MAC\'d Cipher Text: {finalCipher}' )
+##################
+
+
+###   UNMAC    ###
+
+##################
+
+
+### DECRYPTION ###
+
+decipher = Cipher( algorithms.AES( cipherKey ), modes.CTR( nonce ), default_backend() )	# Create the Cipher
+decryptor = decipher.decryptor()
+mt = decryptor.update( ct ) + decryptor.finalize()
+
+print( f'Decrypted Text: {mt.decode()}' )
+
+##################
