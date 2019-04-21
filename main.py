0bb28423dfc55bef3ec1a1dbd6dbd59eb5fbcb09
NickChiapputo <chiapputo.nick@gmail.com>
2019-04-21 05:25:43 -0500

    encrypt() now uploads encrypted data to Drive. decrypt() not downlaods encrypted data from Drive and saves decrypted data locally.

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 from cryptography.hazmat.primitives import hashes, hmac
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
@@ -132,22 +132,25 @@ def generatePBK( salt, length ):
 				backend = default_backend() )											# Use default backend
 
 # # #
+# Paramaters:
+#	service: Service object that references to the user's Drive account
+#
 # This function encrypts a local file. It then uploads the file to the user's google drive as 
 # a plain text file under the same name. The file is encrypted using three user passwords for the
-# cipher and nonce for AES in CTR mode and the MAC key for SH256 HMAC generation. 
+# cipher and nonce for AES in CTR mode and the MAC key for SHA256 HMAC generation. 
 # # #
-def encrypt():
+def encrypt( service ):
 	filename = input( 'File to encrypt: ' ) 											# Get file name from user
 	salt = ( filename + "saltsaltsaltsalt" ).encode()									# Generate unique salt by appending salt to useranme. Not incredibly secure, but more secure than a constant salt and significantly better than no salt
 
 
-	try:
+	if os.path.exists( filename ): 														# Check if file exists. If so, read from file
 		# Get data from file 'thefile'
 		with open( filename, 'rb' ) as f:
 			data = f.read()
-	except:
-		print( ERROR + 'Unable to read from file ' + RESET + filename + '.' )
-		return
+	else:																				# If file doesn't exist, stop encrypting
+		print( ERROR + 'Unable to read from file ' + RESET + filename + '.' ) 			# Tell user file doesn't exists locally
+		return 																			# Return to main method
 
 
 	cKey  = generatePBK( salt, 32 ).derive( input( 'Cipher Key Password: ' ).encode() )	# Create cipher key derivation function then derive key using user given password
@@ -169,31 +172,90 @@ def encrypt():
 
 
 	try:
-		# Write data to file
-		with open( 'thefile', 'wb' ) as f:
+		# Write cipher text to temporary local file
+		with open( 'encrypted_' + filename, 'wb' ) as f:
 			f.write( finalCT )
+
+		file_metadata = { 'name' : filename }											# Set file metadata
+		media = MediaFileUpload( 'encrypted_' + filename, mimetype = 'text/plain' ) 	# Create media upload with mimetype as plain text file
+		file = service.files().create( 	body = file_metadata, 							# Create file on drive with metadata, media, and fields
+										media_body = media,
+										fields = 'id' ).execute()
+
+		os.remove( 'encrypted_' + filename ) 											# Remove temporary local file storing cipher text
+
+		print( 	 'Encrypted data successfully uploaded to Drive. '						# Give user success message
+				f'File name: {filename}' )
 	except:
-		print( '\n\033[1;31mUnable to write to files. '
-				'\033[0mData may be corrupted.\n' )
-	return
+		print( ERROR + '\nUnable to upload encrypted data. ' +							# Tell user encrypted message was not able to upload
+				'Data may be corrupted' + RESET + '.\n' )
+	return 																				# Return to main method
 
 
-def decrypt():
+# # #
+# Paramaters:
+#	service: Service object that references to the user's Drive account
+#
+# This function downloads a specified file from Drive. It then decrypts the file and overwrites
+# the downloaded data in the file. The decryption is done by gatehring three passwords from the
+# user for the cipher and nonce for AES in CTR mode and the MAC key for SHA256 HMAC verification.
+# # #
+def decrypt( service ):
 	filename = input( 'File to encrypt: ' ) 											# Get file name from user
+
+
+	# Search for given file name in Drive
+	page_token = None 																	# Set initial page to None
+	while True:
+		# Search for file by name in drive and store in 'files' value with tuple containing id and name
+		response = service.files().list( q = "name = '{}'".format( filename ),			# Search parameter is name = filename
+										spaces = 'drive',								# Look through drive
+										fields = 'nextPageToken, files(id, name)',		# Use pagetoken and store in 'files' with id and name tuple
+										pageToken = page_token ).execute()				# Set page token and execute search
+        
+
+		# Drive stores files with a unique ID. User can't be expected to know complex ID.
+		# However, there may be multiple of the same file. To get around this, the first file found
+		# given the search parameters is used as the file being searched for as most users will not
+		# create multiple files with the same name. This will cover a significant majority of cases
+		try:
+			file_id = response.get( 'files', [] )[ 0 ].get( 'id' )						# Get file id from first file that matches given file name
+		except:
+			print( ERROR + '\nUnable to download ' + RESET + 							# Tell user file was not found on Drive				
+					filename + ERROR + ' from Drive' + RESET + '.' )	 
+			return 																		# Return to main method
+
+
+		page_token = response.get( 'nextPageToken', None )								# Go to next page
+
+		if page_token is None:															# Break out of while loop if next page does not exist
+			break
+
+	# Call the Drive v3 API
+	request = service.files().get_media( fileId = file_id )								# Create request for file
+	fh = io.FileIO( filename, 'wb' )													# Open file for writing (will overwrite local file if exists)
+	downloader = MediaIoBaseDownload( fh, request )										# Create downloader using request and local file
+	done = False 																		# Initially, file is not done downloading
+
+	# Loop until download is done
+	while done is False:
+		status, done = downloader.next_chunk()											# Get next chunk (smaller files may only use one chunk)
+
+
+	if os.path.exists( filename ):														# Check if file exists. If so, read from file
+		# Read cipher text and mac from file
+		with open( filename, 'rb' ) as f:
+			finalCT = f.read()
+	else:																				# If file doesn't exist, stop decrypting
+		print( ERROR + '\nUnable to read encrypted file' + RESET + '.' )				# Tell user program was unable to read from files
+		return 																			# Return to main method
+
+
 	salt = ( filename + "saltsaltsaltsalt" ).encode()									# Generate unique salt by appending salt to useranme. Not incredibly secure, but more secure than a constant salt and significantly better than no salt
 
 	cKey  = generatePBK( salt, 32 ).derive( input( 'Cipher Key Password: ' ).encode() )	# Create cipher key derivation function then derive key using user given password
 	mKey  = generatePBK( salt, 32 ).derive( input( 'MAC Key Password   : ' ).encode() )	# Create MAC key    derivation function then derive key using user given password
 	nonce = generatePBK( salt, 16 ).derive( input( 'Nonce Password     : ' ).encode() )	# Create nonce key  derivation function then derive key using user given password
-
-
-	try:																				# Attempt to read from files. Catch exception if unable to read
-		# Read cipher text and mac from file
-		with open( filename, 'rb' ) as f:
-			finalCT = f.read()
-	except:
-		print( '\n\033[1;31mUnable to read encrypted file.\033[0m\n' )					# Tell user program was unable to read from files
-		return
 
 
 	ctLen = len( finalCT ) - 32 														# Calculate the length of the cipher text as the total length minus the length of the appended MAC signature
@@ -202,7 +264,7 @@ def decrypt():
 
 	if not verify:
 		print( ERROR + '\nInvalid MAC signature or password' + RESET + '.' )			# Tell the user the MAC signature is bad
-		return
+		return 																			# Return to main method
 
 	mt = AES256decrypt( cKey, nonce, finalCT[ :ctLen ] )								# Decrypt the cipher text using AES256 in CTR mode
 
@@ -245,10 +307,15 @@ def main():
 		with open( 'token.pickle', 'wb' ) as token:										# Open token.pickle for writing
 			pickle.dump( creds, token ) 												# Save information/creds to token.pickle
 
-	service = build( 'drive', 'v3', credentials = creds ) 					# Create the service connected to the user's (given by creds) drive v3 (given by application and version)
+	try:
+		service = build( 'drive', 'v3', credentials = creds ) 							# Create the service connected to the user's (given by creds) drive v3 (given by application and version)
+	except:
+		print( ERROR + 'ERROR:' + RESET + 'Unable to log in to Drive.\n' ) 				# Alert user they are unable to log in to Drive, then terminate program
+		exit()
+
 	about = service.about().get( fields = '*' ).execute()
 
-	print( u'Successfully logged in as {0}.\n'.format( about['user']['displayName'] ) )
+	print( u'Logged in as {0}.\n'.format( about['user']['displayName'] ) )				# Tell user log in is successful. 'user' is a nested object with multiple attributes. Full list is shown in drive api link in README (Reference -> About -> Overview)
 
 
 	while( 1 ):
@@ -261,14 +328,14 @@ def main():
 		userInput = input( '>> ' )														# Prompt for user input
 
 		if userInput.lower()   == 'encrypt' or userInput == '1':
-			encrypt() 																	# Encrypt the thing
+			encrypt( service ) 															# Encrypt the thing
 		elif userInput.lower() == 'decrypt' or userInput == '2':
-			decrypt()																	# Decrypt the thing
+			decrypt( service )															# Decrypt the thing
 		elif userInput.lower() == 'exit' 	or userInput == '3':
 			exit()																		# Leave this thing
 		else:
 			print( ERROR + 'Bad Input' + RESET + '.\n' )								# Tell user bad thing
-		print( '' )																		# Print newline (thing)
+		print( "" )																		# Print newline (thing)
 
 
 # If run from console, start main method
